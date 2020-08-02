38ecb363f9a831b527cb980cf920c1ef7aac25dc
xmrea <46967762+xmrea@users.noreply.github.com>
2020-08-02 18:37:49 +0300

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -24,7 +24,7 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = ""
-    help_string = """"ottomans
+    help_string = """@DRAKE
 Python {}
 Telethon {}
 UserBot Forked from https://github.com/xmrea/uniborg""".format(
